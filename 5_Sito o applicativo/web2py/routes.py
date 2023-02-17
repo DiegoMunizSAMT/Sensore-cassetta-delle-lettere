8484c123da64482521e8e48e223d83784d60686c
AlexIerardi <alex.ierardi@samtrevano.ch>
2023-02-17 13:52:10 +0100

    rename web2py application and routers

---
+++ 5_Sito o applicativo/web2py/routes.py
@@ -0,0 +1,8 @@
+#this file is not reloaded automatically
+#to reload either restart web2py or click "Reload router" in admin page (top right corner)
+
+routes_in=[('/','/scl/default/index')]
+
+#default_application='scl'
+#default_controller = "default"
+#default_function = "index"
